1a2f84c95987a57d7c210092fdb085f1f416e25b
mboxed <mboxxed@gmail.com>
2019-08-11 13:55:33 -0400

    more customization, better functions

--- script/sodatsu.js
+++ script/sodatsu.js
@@ -1,5 +1,7 @@
 var md = window.markdownit({html: true, breaks: true});
 
+const fun = /\{(.*)\}/
+
 window.addEventListener('hashchange', function() {
 	loadPageFromHash(location.hash)
 })
@@ -9,37 +11,31 @@ window.addEventListener('load', function() {
 })
 
 function loadPageFromHash(hash) {
-	if(hash === "") {
-		location.hash = "#home"
-	}
+	try {
+		if(hash === "") {
+			location.hash = "#home"
+		}
 
-	var transformedHash = hash.replace("#","")
-	if(directory[transformedHash] === undefined) {
-
-	} else {
-		var data = directory[transformedHash]
+		var transformedHash = hash.replace("#","")
+		var data = undefined
+		if(directory[transformedHash] === undefined) {
+			data = directory["hidden:404"]
+		} else {
+			var data = directory[transformedHash]
+		}
 
 		document.getElementById("body").innerHTML = ""
 
 		data.forEach( function(e, i) {
-			document.getElementById("body").innerHTML += md.render(e) + "\n"
+			if(fun.test(e)) {
+				document.getElementById("body").innerHTML += md.render(eval(e.match(fun)[1])) + "\n"
+			} else {
+				document.getElementById("body").innerHTML += md.render(e) + "\n"
+			}
 		});
-
-		if(transformedHash == "home") {
-			document.getElementById("body").innerHTML += index()
-		}
 	}
-}
-
-function index() {
-	var list = "<ul>\n"
-	Object.keys(directory).forEach(function(e, i) {
-		list += "<li><a href=\"#" + e + "\">" + e + "</a></li>\n"
-	})
-	list += "</ul>\n"
-	return list
-}
-
-function tableExample() {
-	return "<table><tr><th>test</th><th>test2</th></tr><tr><td>data</td><td>data2</td></tr></table>\n"
+	catch(err) {
+		document.getElementById("body").innerHTML = `<p>Sorry, there was an error processing this page...</p>
+		<p>${err}</p>`
+	}
 }
---
+++ script/transform.js
@@ -0,0 +1,13 @@
+function index() {
+	var list = "<hr>\n<h2>Index</h2>\n<ul>\n"
+	Object.keys(directory).forEach(function(e, i) {
+		if(!e.startsWith("hidden:"))
+			list += `<li><a href="#${e}">${e}</a></li>\n`
+	})
+	list += "</ul>\n<hr>\n"
+	return list
+}
+
+function cannotFind(hash) {
+	return `<p>It looks like the page "${hash}" doesn't exist...`
+}
